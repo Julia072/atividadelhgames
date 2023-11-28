45e21519ca95aac87e02e39b90276b17b09e87ff
Julia <julia.a.silva8@aluno.senai.br>
2023-11-28 17:00:02 -0500

    ainda falhando

--- loja-games/src/app/menu/menu.component.ts
+++ loja-games/src/app/menu/menu.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent {
   
 }
